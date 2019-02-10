ca6474097f28a2a3b2779268318a47f82894cea9
edwinmurillo22 <edwin92nacho@gmail.com>
2019-02-10 12:14:57 -0600

    Creando pagina web home y productos

---
+++ QuieroPizza/QuieroPizza.Web/Controllers/ProductosController.cs
@@ -0,0 +1,25 @@
+﻿using QuieroPizza.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QuieroPizza.Web.Controllers
+{
+    public class ProductosController : Controller
+    {
+        // GET: Productos
+        public ActionResult Index()
+        {
+
+            var producto = new ProductoModel();
+            producto.id = 1;
+            producto.Descripcion = "Pizza 6 Queso";
+
+
+
+            return View(producto);
+        }
+    }
+}
---
+++ QuieroPizza/QuieroPizza.Web/Models/ProductoModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QuieroPizza.Web.Models
+{
+    public class ProductoModel
+    {
+
+        public int id { get; set; }
+        public string Descripcion { get; set; }
+    }
+}

b79fff57669b700b6f6cd3a9512383a5b497f4e1
edwinmurillo22 <edwin92nacho@gmail.com>
2019-02-10 12:33:22 -0600

    Agregando Lista de Productos

--- QuieroPizza/QuieroPizza.Web/Controllers/ProductosController.cs
+++ QuieroPizza/QuieroPizza.Web/Controllers/ProductosController.cs
@@ -13,13 +13,25 @@ namespace QuieroPizza.Web.Controllers
         public ActionResult Index()
         {
 
-            var producto = new ProductoModel();
-            producto.id = 1;
-            producto.Descripcion = "Pizza 6 Queso";
+            var producto1 = new ProductoModel();
+            producto1.id = 1;
+            producto1.Descripcion = "Pizza 6 Queso";
+
+            var producto2 = new ProductoModel();
+            producto2.id = 1;
+            producto2.Descripcion = "Pizza 3 Queso";
+
+            var producto3 = new ProductoModel();
+            producto3.id = 1;
+            producto3.Descripcion = "Pizza Suprema";
 
 
+            var listadeProductos = new List<ProductoModel>();
+            listadeProductos.Add(producto1);
+            listadeProductos.Add(producto2);
+            listadeProductos.Add(producto3);
 
-            return View(producto);
+            return View(listadeProductos);
         }
     }
 }
